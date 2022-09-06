a27051d0e30f6855857f0346fe77c66e119e7290
ChelGame <chel.06081995@gmail.com>
2022-09-06 10:25:09 +0300

    4 module d'not work 2

--- cfg/webpack.client.config.js
+++ cfg/webpack.client.config.js
@@ -5,7 +5,7 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const NODE_ENV = process.env.NODE_ENV;
 const IS_DEV = NODE_ENV === 'development';
 const IS_PROD = NODE_ENV === 'production';
-const GLOBAL_CSS_REGEXP = /\.global\.css/;
+const GLOBAL_CSS_REGEXP = /\.global\.css$/;
 
 function setupDevtool(){
     if(IS_DEV) return 'eval';
@@ -53,7 +53,7 @@ module.exports = {
             },
             {
                 test: GLOBAL_CSS_REGEXP,
-                use: ['css-loader'],
+                use: ['style-loader', 'css-loader'],
             },
         ]
     },
